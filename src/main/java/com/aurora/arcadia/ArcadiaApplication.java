76fd4906a5db93a124f623b8b77038a92b0fc7f6
lfuser <1205027576@qq.com>
2019-09-19 21:10:09 +0800

    Initial commit

---
+++ src/main/java/com/aurora/arcadia/ArcadiaApplication.java
@@ -0,0 +1,13 @@
+package com.aurora.arcadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcadiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcadiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aurora/arcadia/ArcadiaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aurora.arcadia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArcadiaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
