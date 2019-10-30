abd7be9d85e445989a912808d4164a08d8563caa
lfuser <1205027576@qq.com>
2019-10-30 21:05:12 +0800

    lf

--- src/main/java/com/aurora/arcadia/config/MyBatisConfig.java
+++ src/main/java/com/aurora/arcadia/config/MyBatisConfig.java
@@ -9,15 +9,15 @@ import org.springframework.context.annotation.Bean;
  * @create 2019-10-29-下午 9:51
  */
 public class MyBatisConfig {
-    @Bean
-    public ConfigurationCustomizer configurationCustomizer(){
-        return new ConfigurationCustomizer(){
-
-            @Override
-            public void customize(org.apache.ibatis.session.Configuration configuration) {
-                //驼峰命名规则
-                configuration.setMapUnderscoreToCamelCase(true);
-            }
-        };
-    }
+//    @Bean
+//    public ConfigurationCustomizer configurationCustomizer(){
+//        return new ConfigurationCustomizer(){
+//
+//            @Override
+//            public void customize(org.apache.ibatis.session.Configuration configuration) {
+//                //驼峰命名规则
+//                configuration.setMapUnderscoreToCamelCase(true);
+//            }
+//        };
+//    }
 }
