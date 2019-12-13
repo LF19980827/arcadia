1c30e14fbe7eb9fbf0a05ebfc07484abcd32b90b
Benjamin <305193188@qq.com>
2019-12-13 11:49:14 +0800

    发帖功能分支

--- src/main/java/com/aurora/arcadia/controller/PostController.java
+++ src/main/java/com/aurora/arcadia/controller/PostController.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -38,6 +39,7 @@ public class PostController {
      * @return 封装的JSON格式响应信息
      */
     @ResponseBody
+    @RequestMapping(value = "/submitNewPost")
     public Map<String, Object> submitNewPost(@RequestParam(value = "postTitle", required = true)String postTitle,
                                              @RequestParam(value = "postIntro", required = true)String postIntro,
                                              @RequestParam(value = "postUserId", required = false)Integer postUserId,
